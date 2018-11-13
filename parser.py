f331bef58c109bd486051361beb498d9bb848afb
Ethan <etjohn2345@hotmail.com>
2018-11-12 20:08:24 -0500

    updated parser

--- parser.py
+++ parser.py
@@ -1,19 +1,29 @@
 import numpy as np
+from os import listdir
+from os.path import isfile, join
 
-def parse_chat(chat_file):
+def parse_chat(chat_file, raw_path):
     #chat_file = "mfbiscuits.txt"
 
-    file_name = chat_file + "--parsed.txt"
-    parsed_chat = open(file_name, "w")
-    with open(chat_file) as fp:
+    chat_name = chat_file.lstrip(raw_path)
+    file_name = "parsed_data/" + chat_name + "-parsed.txt"
+    parsed_chat = open(file_name, "w", encoding='utf-8')
+    with open(chat_file, encoding='utf-8') as fp:
         line = fp.readline()
         while line:
-            #line[11:] truncates the timestamp
-            if(line[11] == '<'):
+            
+            '[*] <*> +'
+            if( == ):
+                print("TRUE")
                 statement_with_usernames = "{}".format(line[11:].strip())
                 start_index = statement_with_usernames.find('>')
                 statement = statement_with_usernames[start_index+1:]
-                parsed_chat.write(statement + '\n')
+                parsed_chat.write(statement.lower() + '\n')
             line = fp.readline()
+
 if __name__ == '__main__':
-	parse_chat("mfbiscuits.txt")
+    mypath = 'raw_data/'
+    filenames = np.array([(mypath + f) for f in listdir(mypath) if isfile(join(mypath, f))])
+    #print(filenames)
+    for file in filenames:
+        parse_chat(file, mypath)
