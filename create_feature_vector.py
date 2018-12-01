1500e5956348b654fdab75f67675dcc3d13decaf
Chigozie Ikoro <chigozie.ikoro@gmail.com>
2018-12-01 15:28:31 -0500

    Updated features for feature vector.

---
+++ create_feature_vector.py
@@ -0,0 +1,107 @@
+## Input file should be in the form:
+## vod ID
+## text line 1
+## text line 2
+## ...
+## empty line
+## vod ID (Repeat)...
+
+def index_text(chat_file): # uses the all-parsed file
+    # index shape: vod[(vod_ID, chat[line[text]])]
+    vods = []
+    chat_index = []
+    
+    with open(chat_file, 'r', encoding='utf-8') as fp:
+        vod_id = fp.readline() # first line is vod ID
+        line = fp.readline()
+        
+        line_num = 0
+        vod_count = 0
+        while line:
+            if line == '\n': # empty line, end of previous vod
+                line = fp.readline()
+                if line: # line has ID of vod
+                    vod_count += 1
+                    vods.append((vod_id, chat_index))
+                    vod_id = line
+                    
+                    line = fp.readline()
+                    chat_index = []
+                    line_num = 0
+                else:
+                    break # eof
+            
+            li = line.split() # make list of words
+            
+            chat_index.append(li) # add list of words to index
+
+            line = fp.readline()
+            line_num += 1
+        vods.append((vod_id, chat_index)) # put last vod in the index
+    return vods
+
+def create_fv(indx, label):
+    fv = []
+    f_names = [] # list of names of features
+
+    f_names.append('exclaiming')
+    f_names.append('questioning')
+    f_names.append('sentiment score')
+    
+    f_names.append('label')
+
+    fv.append(f_names)
+    
+    for line in indx: # (vod_id, chat_info)
+        fv.append(_compute_features(line, label))
+    return fv
+    
+
+def _compute_features(line, label):
+    vec = []
+    vec.append(_endswith_exclaim(line))
+    vec.append(_endswith_question(line))
+    vec.append(_sentiment_score(line))
+    # ADD MORE FEATURE FUNCTION CALLS HERE
+    vec.append(label)
+    return vec
+
+# FEATURES GO HERE
+# All feature values should be in the range 0 <= x < inf
+#return 1 if line ends with exclamation point. 0 if not.
+def _endswith_exclaim(line): # TODO
+    if line[-1] == '!':
+        return 1
+    return 0
+
+def _endswith_question(line): # TODO
+    if line[-1] == '?':
+        return 1
+    return 0
+
+def _sentiment_score(line): # TODO
+    return 0
+
+def output_features(neg_fv, pos_fv, of): # fv = feature vector, of = output file
+    # file.write(neg_fv + pos_fv)
+    file = open(of, 'w')
+    for vec in neg_fv:
+        line = ''
+        for f in vec:
+            line += str(f) + ', '
+        file.write(line[:-2] + '\n') # remove last comma and space
+    for vec in pos_fv[1:]:
+        line = ''
+        for f in vec:
+            line += str(f) + ', '
+        file.write(line[:-2] + '\n') # remove last comma and space
+
+if __name__ == '__main__':
+    neg_data_file = 'data/labelled_data/0.txt'
+    pos_data_file = 'data/labelled_data/1.txt'
+    neg_indx = index_text(neg_data_file)
+    pos_indx = index_text(pos_data_file)
+    neg_fv = create_fv(neg_indx, 0)
+    pos_fv = create_fv(pos_indx, 1)
+    of = 'data/data.csv'
+    output_features(neg_fv, pos_fv, of)
--- parser.py
+++ parser.py
@@ -1,19 +1,153 @@
 import numpy as np
+from os import listdir
+from os.path import isfile, join
+import re
+import operator
 
-def parse_chat(chat_file):
+def parse_chat(chat_file, raw_path):
     #chat_file = "mfbiscuits.txt"
 
-    file_name = chat_file + "--parsed.txt"
-    parsed_chat = open(file_name, "w")
-    with open(chat_file) as fp:
+    chat_name = chat_file.lstrip(raw_path)
+    file_name = "data/parsed_data/" + chat_name + "-parsed.txt"
+    parsed_chat = open(file_name, "w", encoding='utf-8')
+    with open(chat_file, encoding='utf-8') as fp:
         line = fp.readline()
         while line:
-            #line[11:] truncates the timestamp
-            if(line[11] == '<'):
-                statement_with_usernames = "{}".format(line[11:].strip())
-                start_index = statement_with_usernames.find('>')
-                statement = statement_with_usernames[start_index+1:]
-                parsed_chat.write(statement + '\n')
+            message = "".join(line.split('> ')[1:]) # text starts after the first occurrence
+            parsed_chat.write(message.lower())
             line = fp.readline()
+            
+    parsed_chat.close()
+
+def parse_all_chats(chat_files, raw_path):
+    #Format:
+    #line 1: name of vod
+    #subsequent lines until blank line: lines of vod
+    #next line with text: name of next vod
+    
+    file_name = "data/parsed_data/all-parsed.txt"
+    parsed_chat = open(file_name, "w", encoding='utf-8')
+    for chat_file in chat_files:
+        chat_name = chat_file.lstrip(raw_path)[:-4]
+        
+        with open(chat_file, encoding='utf-8') as fp:
+            parsed_chat.write(chat_name + '\n')
+            line = fp.readline()
+            while line:
+                message = "".join(line.split('> ')[1:]) # text starts after the first occurrence
+                parsed_chat.write(message.lower())
+                line = fp.readline()
+            parsed_chat.write('\n') # empty line to denote end of file
+        
+    parsed_chat.close()
+
+def index_text(chat_file): # uses all-parsed file
+    # index shape: vod{vod_name:chat[line[text]]}
+    vods = {}
+    chat_index = []
+    
+    with open(chat_file, 'r', encoding='utf-8') as fp:
+        vod_name = fp.readline() # first line is vod name
+        line = fp.readline()
+        
+        line_num = 0
+        vod_count = 0
+        while line:
+            if line == '\n': # empty line, end of previous vod
+                line = fp.readline()
+                if line: # line has name of vod
+                    vod_count += 1
+                    vods[vod_name] = chat_index
+                    vod_name = line
+                    
+                    line = fp.readline()
+                    chat_index = []
+                    line_num = 0
+                else:
+                    break # eof
+            
+            li = line.split() # make list of words
+            
+            chat_index.append(li) # add list of words to index
+
+            line = fp.readline()
+            line_num += 1
+        vods[vod_name] = chat_index # put last vod in the index
+    return vods
+    
+def word_counts(index):
+    wl = {} # word list dictionary
+    for vod in index.values():
+        for message in vod:
+            for word in message:
+                # parse words to remove commas
+                word = check_word(word)
+                word = word.split('/')
+                for w in word:
+                    if w == '':
+                        continue
+                    if w not in wl.keys():
+                        wl[w] = 1
+                    else:
+                        wl[w] = wl[w] + 1
+    return wl
+
+def check_word(w):
+    #stopwords = []
+    olws = re.compile('[a-z0-9\^@]') # valid one-letter words
+    http = re.compile('.*https?://.+') # check for http link
+    marks = re.compile('[^a-z0-9\-_\'\\/]') # contains certain marks
+    initials = re.compile('[a-z]\.[a-z]\.+') # initialism
+    
+    #print(w)
+    if(len(w) == 1):
+        if(not olws.match(w)):
+            return ''
+        else: return w
+
+    if(http.match(w)):
+        return ''
+        
+    ms = marks.finditer(w)
+    i = 0
+    for mi in ms:
+        mi = mi.start()
+        if(len(w)-1 == mi or not initials.match(w[mi-i:])):
+            #print("mark found at index", mi-i)
+            if(mi == 0):
+                w = w[1:]
+            else:
+                w = w[:mi-i] + w[mi+1-i:]
+            #print(w)
+        i += 1
+    return w
+    
+##def check_words(w):
+##    thisorthat = re.compile('\w+/\w+') # ex: this/that
+##    if thisorthat.match(w)
+##        return w.split('/') # IMPROVE: doesn't consider edge cases
+##    else: return [w]
+
+def wl_sorted_output(wl):
+    #sort it from most common word to least common word
+    sorted_wl = sorted(wl.items(), key=operator.itemgetter(1), reverse=True)
+    wlf = open('word_counts.csv', 'w', encoding='utf-8')
+    for word in sorted_wl:
+        output = str(word[0]) + ', ' + str(word[1]) + '\n'
+        #print(output)
+        wlf.write(output)
+    wlf.close()
+    
+            
+
 if __name__ == '__main__':
-	parse_chat("mfbiscuits.txt")
+    #mypath = 'raw_data/'
+    #filenames = np.array([(mypath + f) for f in listdir(mypath) if isfile(join(mypath, f))])
+    #print(filenames)
+    #parse_all_chats(filenames, mypath)
+    indx = index_text('data/all-parsed.txt') # Get inverted index of vods
+    wl = word_counts(indx) # get the word counts of each word
+    wl_sorted_output(wl) # output sorted word counts to predetermined file
+    #for file in filenames:
+        #parse_chat(file, mypath)
+        #index_text(file, mypath)
