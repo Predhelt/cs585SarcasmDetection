998832003d665087db0ba3d3bd4850ce5dae36b6
Ethan <etjohn2345@hotmail.com>
2018-12-19 23:01:27 -0500

    added cross validation

--- detect_sarcasm.py
+++ detect_sarcasm.py
@@ -1,9 +1,11 @@
 # Uses the data.csv file to train the classifiers
+import numpy as np
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
 
 def get_data(file):
     # read the data from the data.csv file line-by-line
@@ -21,24 +23,38 @@ def get_data(file):
         X.append(list(map(float, vs[:-1])))
         y.append(int(vs[-1]))
     
-    return X, y, feats
+    return np.array(X), np.array(y), feats
 
 # TODO: tweak parameters
 def nn_clf():
 
-    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                    hidden_layer_sizes=(5, 2), random_state=1)
+##    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+##                    hidden_layer_sizes=(3, 2), random_state=1)
     
-##    MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
-##                  beta_1=0.9, beta_2=0.999, early_stopping=False,
-##                  epsilon=1e-08, hidden_layer_sizes=(5, 2),
-##                  learning_rate='constant', learning_rate_init=0.001,
-##                  max_iter=200, momentum=0.9, n_iter_no_change=10,
-##                  nesterovs_momentum=True, power_t=0.5, random_state=1,
-##                  shuffle=True, solver='lbfgs', tol=0.0001,
-##                  validation_fraction=0.1, verbose=False, warm_start=False)
+    clf = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+                  beta_1=0.9, beta_2=0.999, early_stopping=False,
+                  epsilon=1e-08, hidden_layer_sizes=(3, 2),
+                  learning_rate='constant', learning_rate_init=0.001,
+                  max_iter=200, momentum=0.9, nesterovs_momentum=True, power_t=0.5, random_state=1,
+                  shuffle=True, solver='lbfgs', tol=0.0001,
+                  validation_fraction=0.1, verbose=False, warm_start=False)
     return clf
 
+def cross_validate(clf, X, y):
+    kf = KFold(n_splits=2)
+    i = 1
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        print('fold ', i)
+        print('confusion matrix:\n', confusion_matrix(y_test, y_pred, [0,1]))
+        print('precision:\n', precision_score(y_test, y_pred, [0,1]))
+        print('recall:\n', recall_score(y_test, y_pred, [0,1]))
+        print('accuracy:\n', accuracy_score(y_test, y_pred, [0,1]))
+        print('---------------------')
+        i+=1
 
 if __name__ == '__main__':
     testf = 'data/data.csv'
@@ -48,6 +64,8 @@ if __name__ == '__main__':
 
     print("setting up classifier")
     clf = nn_clf()
+##    print("running cross validation")
+##    cross_validate(clf, X, y)
     print("fitting classifier")
     clf.fit(X, y)
 
